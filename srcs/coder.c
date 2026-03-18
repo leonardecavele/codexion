65e7c4fac2734ccd8592f6ca66fe6efdcfb6ac76
leonardecavele <leonard.decavele@gmail.com>
2026-03-18 15:47:03 +0100

    fix: data race

--- srcs/coder.c
+++ srcs/coder.c
@@ -6,7 +6,7 @@
 /*   By: ldecavel <ldecavel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/16 17:40:19 by ldecavel          #+#    #+#             */
-/*   Updated: 2026/03/18 15:38:10 by ldecavel         ###   ########.fr       */
+/*   Updated: 2026/03/18 15:46:12 by ldecavel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,51 +28,45 @@ static void	exec_activity(
 	usleep(time_to_wait * 1000);
 }
 
-// issue here
 static void	take_dongles(
-	t_coder *coder, t_dongle *dongle_1, t_dongle *dongle_2
+	t_coder *coder, t_dongle *dongle_1, t_dongle *dongle_2, t_session *session
 )
 {
-	pthread_mutex_lock(&coder->session->dongles_mutex);
-	while (!(dongle_1->available && dongle_2->available))
-		pthread_cond_wait(
-			&coder->session->dongles_cond, &coder->session->dongles_mutex
-		);
-	pthread_mutex_unlock(&coder->session->dongles_mutex);
-	while (
-		!((elapsed_time_ms(dongle_1->last_use) >= coder->args->dc
-		 && elapsed_time_ms(dongle_2->last_use) >= coder->args->dc))
-	)
+	while (1)
+	{
+		pthread_mutex_lock(&session->dongles_mutex);
+		while (!(dongle_1->available && dongle_2->available))
+			pthread_cond_wait(&session->dongles_cond, &session->dongles_mutex);
+		if (elapsed_time_ms(dongle_1->last_use) >= coder->args->dc
+			&& elapsed_time_ms(dongle_2->last_use) >= coder->args->dc)
+		{
+			dongle_1->available = false;
+			dongle_2->available = false;
+			pthread_mutex_unlock(&session->dongles_mutex);
+			break ;
+		}
+		pthread_mutex_unlock(&session->dongles_mutex);
 		usleep(100);
-	pthread_mutex_lock(&coder->session->dongles_mutex);
-	dongle_1->available = false;
-	dongle_2->available = false;
-	exec_activity(coder->session->start_ms, "has taken a dongle", coder, 0);
-	exec_activity(coder->session->start_ms, "has taken a dongle", coder, 0);
-	pthread_mutex_unlock(&coder->session->dongles_mutex);
+	}
+	exec_activity(session->start_ms, "has taken a dongle", coder, 0);
+	exec_activity(session->start_ms, "has taken a dongle", coder, 0);
 }
 
-static void	routine(t_coder *coder)
+static void	routine(t_coder *coder, t_args *args, t_session *session)
 {
 	pthread_mutex_lock(&coder->last_compile_mutex);
 	coder->last_compile = current_time_ms();
 	pthread_mutex_unlock(&coder->last_compile_mutex);
-	exec_activity(
-		coder->session->start_ms, "is compiling", coder, coder->args->ttc
-	);
+	exec_activity(session->start_ms, "is compiling", coder, args->ttc);
 	coder->left->last_use = current_time_ms();
 	coder->right->last_use = current_time_ms();
-	pthread_mutex_lock(&coder->session->dongles_mutex);
+	pthread_mutex_lock(&session->dongles_mutex);
 	coder->left->available = true;
 	coder->right->available = true;
-	pthread_cond_broadcast(&coder->session->dongles_cond);
-	pthread_mutex_unlock(&coder->session->dongles_mutex);
-	exec_activity(
-		coder->session->start_ms, "is debugging", coder, coder->args->ttd
-	);
-	exec_activity(
-		coder->session->start_ms, "is refactoring", coder, coder->args->ttr
-	);
+	pthread_cond_broadcast(&session->dongles_cond);
+	pthread_mutex_unlock(&session->dongles_mutex);
+	exec_activity(session->start_ms, "is debugging", coder, args->ttd);
+	exec_activity(session->start_ms, "is refactoring", coder, args->ttr);
 }
 
 extern void	*handle_coder(void *arg)
@@ -88,10 +82,10 @@ extern void	*handle_coder(void *arg)
 	while (++i < coder->args->nocr)
 	{
 		if (coder->id % 2 == 0)
-			take_dongles(coder, coder->left, coder->right);
+			take_dongles(coder, coder->left, coder->right, coder->session);
 		else
-			take_dongles(coder, coder->right, coder->left);
-		routine(coder);
+			take_dongles(coder, coder->right, coder->left, coder->session);
+		routine(coder, coder->args, coder->session);
 	}
 	pthread_mutex_lock(&coder->over_mutex);
 	coder->over = true;
